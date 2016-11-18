7e14cfc49d82ff531158af4014eb61b5cc413da2
Karan Jain <karanj@thoughtworks.com>
2016-11-18 17:56:43 +0530

    Designed class layout for nucleotide

---
+++ nucleotide-count/src/main/java/DNA.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class DNA {
+
+
+  public DNA(String s) {
+
+  }
+
+  public Integer count(char a) {
+    return 0;
+  }
+
+  public List nucleotideCounts() {
+    return null;
+  }
+}
